bc85d7477b1fb5e2824c2565b07691955e68c0ae
abonalumi <augustobonalumi@hotmail.com>
2022-08-30 20:16:54 -0300

    Subscriptor
    Se agrega lógica para leer los mensajes publicados en la cola del canal "Saludo"

--- RabbitMQpractica/Publicador/Emisor.cs
+++ RabbitMQpractica/Publicador/Emisor.cs
@@ -6,17 +6,20 @@ var factory = new ConnectionFactory() { HostName = "localhost" };
 using(var connection = factory.CreateConnection())
 {
     //se crea un canal
-    using(var channel = connection.CreateModel())
+    using(var canal = connection.CreateModel())
     {
         // el canal permite que los mensajes lleguen a la cola Queue. Si la cola de mensaje no existe, se crea.
-        channel.QueueDeclare(queue: "Saludo", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        canal.QueueDeclare(queue: "Saludo", durable: false, exclusive: false, autoDelete: false, arguments: null);
         // se crea un mensaje
         string mensaje = "Hola conejo!";
         // se codifica el mensaje a bytes
         var cuerpo = Encoding.UTF8.GetBytes(mensaje);
         // se envía el mensaje
-        channel.BasicPublish(exchange: "", routingKey: "Saludo", basicProperties: null, body: cuerpo);
+        canal.BasicPublish(exchange: "", routingKey: "Saludo", basicProperties: null, body: cuerpo);
         // visualizar el mensaje enviado en consola para ver si la línea de ejecución llegó hasta este punto y qué se envió.
         Console.WriteLine($"[x] {mensaje}");
     }
 }
+
+Console.WriteLine("Presione una tecla para salir.");
+Console.ReadLine();
--- RabbitMQpractica/Subscriptor/Receptor.cs
+++ RabbitMQpractica/Subscriptor/Receptor.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using(var conexion = factory.CreateConnection())
+{
+	using (var canal = conexion.CreateModel())
+	{
+		canal.QueueDeclare(queue: "Saludo", durable: false, autoDelete: false, arguments: null);
+
+		var consumidor = new EventingBasicConsumer(canal);
+
+		consumidor.Received += (model, ea) =>
+		{
+			var cuerpo = ea.Body.ToArray();
+			var mensaje = Encoding.UTF8.GetString(cuerpo);
+
+			Console.WriteLine($"[x] {mensaje}");
+		};
+
+		canal.BasicConsume(queue:"Saludo",autoAck:true, consumer:consumidor);
+	}
+}
+
+Console.WriteLine("Presione una tecla para salir.");
+Console.ReadLine();
